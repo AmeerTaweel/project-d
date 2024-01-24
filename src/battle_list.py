179ee7f27f560b7a0cd1fff8694bcd78a88c9cc1
Ameer Taweel <ameertaweel2002@gmail.com>
2024-01-24 15:46:52 +0300

    fix: small bug

--- src/battle_list.py
+++ src/battle_list.py
@@ -8,7 +8,7 @@ def battle_list(db_name, db_connection):
     db_cursor.execute(f"""
         SELECT R1.*, R2.*, D1.Name, D2.Name, C.Name
         FROM Records AS R1, Drivers AS D1, Records AS R2, Drivers AS D2, Courses AS C
-        WHERE R1.ID <> R2.ID
+        WHERE R1.ID > R2.ID
         AND R1.DriverID = D1.ID
         AND R2.DriverID = D2.ID
         AND R1.CourseID = C.ID
